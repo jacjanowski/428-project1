b4d0b26ea525a309db5c751ed0271f0bd6be9489
Jacob Janowski <60745870+jacjanowski@users.noreply.github.com>
2021-09-16 12:22:52 -0500

    add music source to model. Onto app deployment.

---
+++ Assets/Rotation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+
+    public Vector3 currentRotation;
+    public Vector3 anglesToRotate;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        currentRotation = new Vector3(currentRotation.x % 360f, currentRotation.y % 360f, currentRotation.z % 360f);
+        this.transform.eulerAngles = currentRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentRotation = currentRotation + anglesToRotate * Time.deltaTime;
+        currentRotation = new Vector3(currentRotation.x % 360f, currentRotation.y % 360f, currentRotation.z % 360f);
+        this.transform.eulerAngles = currentRotation;
+
+
+
+
+        
+
+    }
+
+}
